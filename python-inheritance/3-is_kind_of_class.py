0f0f7aaf570a1ba26e9dc7ac68813ed363d8de92
David Pontient Kayumba <d.kayumba1@alustudent.com>
2024-11-07 23:04:14 -0800

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A script that checks if the object is an instance of a class'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Check if the object is an instance then returns True if so
+    '''
+    return isinstance(obj, a_class)
