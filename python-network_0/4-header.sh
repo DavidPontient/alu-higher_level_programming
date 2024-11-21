af85aaf3957297c0b0b464b78c9975c85e52c776
David Pontient Kayumba <d.kayumba1@alustudent.com>
2024-11-21 03:42:29 -0800

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request with the custom header and display the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sG "$1" -H "X-School-User-Id: 98"
