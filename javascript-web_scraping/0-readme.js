afce6191e2746429ee2add6260e9588b514dc429
Kayumba David Pontient <d.kayumba1@alustudent.com>
2025-02-05 13:43:08 +0000

    0-readme.js

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+t request = require('fs');
+
+const filePath = process.argv[2];
+
+request.readFile(filePath, function (err, data) {
+  if (err) console.log(err);
+  const content = data;
+  console.log(content.toString());
+});
